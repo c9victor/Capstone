3ba0a578d16e20bdfcbdf80b5f87ba71a4af7985
c9victor <56898542+c9victor@users.noreply.github.com>
2021-09-16 02:08:49 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,15 +3,19 @@ import pygame
 import random  # added by me
 import csv  # added by me
 import numpy as np  # added by me
+from itertools import product  # needed for dlx implementation
 from solver import solve, valid
 import time
 pygame.font.init()
 
 
 class Grid:
-    # To change the starting board change this
-    # Note that the first line of the file contains headers, do not attempt to parse it as a puzzle
-    with open('.\Puzzles\sudoku.csv', mode='r') as file:
+    '''
+    Opens a csv file filled with sudoku puzzles. The file contains two columns: starting puzzle and finished puzzle.
+    The information in the starting puzzle column is parsed so that we can create a sudoku board.
+    Note that the first line of the file contains headers, do not attempt to parse it as a puzzle
+    '''
+    with open('sudoku.csv', mode='r') as file:
         csvFile = csv.reader(file)
         puzzleNum = random.randint(1, 1000001)
         #print("Puzzle Number:", puzzleNum)
@@ -27,14 +31,14 @@ class Grid:
     #print("reshape():\n", start_puzzle)
     board = start_puzzle
 
-    ''' 
+    
     ### For debugging purposes
     finish_puzzle = list(puzzle[1])
     finish_puzzle.pop()  # needed because there is a '\n' at the end of the list
     finish_puzzle = [int(numeric_string) for numeric_string in finish_puzzle]
     finish_puzzle = np.reshape(finish_puzzle, (9, 9))
     print("finished:\n", finish_puzzle)
-    '''
+    
 
     def __init__(self, rows, cols, width, height):
         self.rows = rows
@@ -46,7 +50,7 @@ class Grid:
         self.selected = None
 
     '''
-    New method. Used to reset the board when three mistakes are made.
+    New method. Used to reset the board when three mistakes are made or when dlx button is clicked.
     '''
     def reset(self):
         self.board = self.start_puzzle
@@ -103,6 +107,11 @@ class Grid:
         if self.cubes[row][col].value == 0:
             self.cubes[row][col].set_temp(0)
 
+    # new overloaded method for DLX deselect
+    def clear(self, row, col):
+        #if self.cubes[row][col].value == 0:
+        self.cubes[row][col].set_temp(0)
+
     def click(self, pos):
         """
         :param: pos - position clicked
@@ -122,6 +131,82 @@ class Grid:
                 if self.cubes[i][j].value == 0:
                     return False
         return True
+
+    def dlx_solve_sudoku(self, size, grid):
+        """ An efficient Sudoku solver using Algorithm X.
+        
+        >>> for solution in dlxSolve((3, 3), grid):
+                print(*solution, sep='\\n')
+        """
+        R, C = size
+        N = R * C
+        X = ([("rc", rc) for rc in product(range(N), range(N))] +
+            [("rn", rn) for rn in product(range(N), range(1, N + 1))] +
+            [("cn", cn) for cn in product(range(N), range(1, N + 1))] +
+            [("bn", bn) for bn in product(range(N), range(1, N + 1))])
+        Y = dict()
+        for r, c, n in product(range(N), range(N), range(1, N + 1)):
+            b = (r // R) * R + (c // C) # Box number
+            Y[(r, c, n)] = [
+                ("rc", (r, c)),
+                ("rn", (r, n)),
+                ("cn", (c, n)),
+                ("bn", (b, n))]
+        
+        ### test code
+        #print("X ", X[0])
+        #res = next(iter(Y))
+        #print("The first key of dictionary is : " + str(res))
+
+        X, Y = self.dlx_exact_cover(X, Y)
+        for i, row in enumerate(grid):
+            for j, n in enumerate(row):
+                if n:
+                    self.dlx_select(X, Y, (i, j, n))
+        for solution in self.dlx_solve(X, Y, []):
+            for (r, c, n) in solution:
+                grid[r][c] = n
+            yield grid
+
+    def dlx_exact_cover(X, Y): #board.place(board.cubes[i][j].temp)
+        X = {j: set() for j in X}
+        for i, row in Y.items():
+            for j in row:
+                X[j].add(i)
+        return X, Y
+
+    def dlx_solve(self, X, Y, solution):
+        if not X:
+            yield list(solution)
+        else:
+            c = min(X, key=lambda c: len(X[c]))
+            for r in list(X[c]):
+                solution.append(r)
+                cols = self.dlx_select(X, Y, r)
+                self.board.place(self.board.cubes[r][c].temp)  # test
+                for s in self.dlx_solve(X, Y, solution):
+                    yield s
+                self.dlx_deselect(X, Y, r, cols)
+                self.clear(r, cols)  # test
+                solution.pop()
+
+    def dlx_select(X, Y, r):
+        cols = []
+        for j in Y[r]:
+            for i in X[j]:
+                for k in Y[i]:
+                    if k != j:
+                        X[k].remove(i)
+            cols.append(X.pop(j))
+        return cols
+
+    def dlx_deselect(X, Y, r, cols):
+        for j in reversed(Y[r]):
+            X[j] = cols.pop()
+            for i in X[j]:
+                for k in Y[i]:
+                    if k != j:
+                        X[k].add(i)
 ### End Of Grid Class
 
 class Cube:
@@ -172,14 +257,14 @@ def redraw_window(win, board, time, strikes):
     win.blit(text, (20, 750))
 
     #experimental code
-    top_height = 600
+    button_top = 600
     button_height = 30
-    pygame.draw.rect(win, (216, 191, 216), [255, top_height, 170, button_height])  # (surface, (r, g, b), [left, top, width, height]) 
+    pygame.draw.rect(win, (216, 191, 216), [120, button_top, 170, button_height])  # (surface, (r, g, b), [left, top, width, height]) 
     text = fnt.render('New Puzzle' , True , (0, 0, 0))
-    win.blit(text, (260, top_height))
-    pygame.draw.rect(win, (216, 191, 216), [255, top_height + 50, 300, button_height])  # (surface, (r, g, b), [left, top, width, height]) 
+    win.blit(text, (120, button_top))
+    pygame.draw.rect(win, (216, 191, 216), [120, button_top + 50, 375, button_height])  # (surface, (r, g, b), [left, top, width, height]) 
     text = fnt.render('Watch Dancing Links Do It!' , True , (0, 0, 0))
-    win.blit(text, (260, top_height + 50))
+    win.blit(text, (120, button_top + 50))
 
     # Draw grid and board
     board.draw(win)
@@ -254,11 +339,21 @@ def main():
                             #run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
+                pos = pygame.mouse.get_pos()  # (x, y) of mouse position
                 clicked = board.click(pos)
                 if clicked:
                     board.select(clicked[0], clicked[1])
                     key = None
+                elif 120 <= pos[0] <= 495 and 650 <= pos[1] <= 680:
+                    board.reset()
+                    #yielded = [board.dlx_solve_sudoku((3, 3), board.board)]
+                    print("DLX Button Clicked!!!")
+                    board.dlx_solve_sudoku((3, 3), board.board)
+                    #for solution in board.dlx_solve_sudoku((3, 3), board.board):
+                    #    print("solved")
+                    #    print(solution)
+                    #print(yielded)
+
 
         if board.selected and key != None:
             board.sketch(key)
