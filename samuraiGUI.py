fcb9c21ba239a08dc57d0d7dfce1a5778f473f51
c9victor <56898542+c9victor@users.noreply.github.com>
2021-11-18 08:36:24 -0500

    Update samuraiGUI.py

--- samuraiGUI.py
+++ samuraiGUI.py
@@ -1,4 +1,4 @@
-# GUI.py - RUN THIS FILE
+# samuraiGUI.py - RUN THIS FILE
 import puzzle_retriever  # our file
 from puzzle_retriever import get_puzzle
 import dlx  # our file
@@ -11,27 +11,49 @@ pygame.font.init()
 class Grid:
     board = get_puzzle(1)
 
-    board1 = [[0, 5, 1, 0, 6, 7, 2, 0, 8, 0, 0, 0, 9, 0, 8, 5, 6, 0, 7, 2, 0],
-              [8, 7, 0, 0, 3, 0, 0, 0, 5, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 5, 3],
-              [6, 0, 0, 0, 0, 8, 7, 0, 0, 0, 0, 0, 0, 0, 6, 4, 0, 0, 0, 0, 1],
-              [0, 1, 8, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 2, 8, 0],
-              [5, 2, 7, 0, 1, 0, 8, 4, 6, 0, 0, 0, 8, 3, 5, 0, 9, 0, 4, 1, 7],
-              [0, 0, 0, 8, 0, 0, 1, 7, 0, 0, 0, 0, 0, 6, 4, 0, 0, 1, 0, 0, 0],
-              [0, 0, 3, 6, 0, 0, 0, 0, 7, 0, 5, 0, 6, 0, 0, 0, 0, 5, 3, 0, 0],
-              [2, 0, 0, 0, 8, 0, 0, 5, 9, 0, 6, 0, 1, 4, 0, 0, 3, 0, 0, 0, 5],
-              [7, 0, 5, 3, 2, 0, 6, 8, 0, 9, 0, 4, 0, 7, 3, 0, 2, 9, 1, 0, 8],
-              [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 6, 0, 9, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0],
-              [9, 0, 6, 3, 4, 0, 5, 7, 0, 6, 0, 3, 0, 2, 1, 0, 3, 5, 7, 0, 8],
-              [4, 0, 0, 0, 1, 0, 0, 6, 2, 0, 8, 0, 4, 3, 0, 0, 1, 0, 0, 0, 5],
-              [0, 0, 2, 6, 0, 0, 0, 0, 3, 0, 9, 0, 8, 0, 0, 0, 0, 4, 3, 0, 0],
-              [0, 0, 0, 2, 0, 0, 4, 9, 0, 0, 0, 0, 0, 7, 5, 0, 0, 2, 0, 0, 0],
-              [6, 9, 1, 0, 3, 0, 2, 5, 7, 0, 0, 0, 3, 1, 4, 0, 6, 0, 5, 8, 2],
-              [0, 7, 4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 4, 6, 0],
-              [1, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 0, 0, 0, 3, 7, 0, 0, 0, 0, 4],
-              [7, 4, 0, 0, 2, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 7, 3],
-              [0, 2, 8, 0, 6, 3, 7, 0, 9, 0, 0, 0, 7, 0, 2, 1, 4, 0, 9, 5, 0]]
+    samurai_board = [[0, 5, 1, 0, 6, 7, 2, 0, 8, 0, 0, 0, 9, 0, 8, 5, 6, 0, 7, 2, 0],
+                    [8, 7, 0, 0, 3, 0, 0, 0, 5, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 5, 3],
+                    [6, 0, 0, 0, 0, 8, 7, 0, 0, 0, 0, 0, 0, 0, 6, 4, 0, 0, 0, 0, 1],
+                    [0, 1, 8, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 2, 8, 0],
+                    [5, 2, 7, 0, 1, 0, 8, 4, 6, 0, 0, 0, 8, 3, 5, 0, 9, 0, 4, 1, 7],
+                    [0, 0, 0, 8, 0, 0, 1, 7, 0, 0, 0, 0, 0, 6, 4, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 3, 6, 0, 0, 0, 0, 7, 0, 5, 0, 6, 0, 0, 0, 0, 5, 3, 0, 0],
+                    [2, 0, 0, 0, 8, 0, 0, 5, 9, 0, 6, 0, 1, 4, 0, 0, 3, 0, 0, 0, 5],
+                    [7, 0, 5, 3, 2, 0, 6, 8, 0, 9, 0, 4, 0, 7, 3, 0, 2, 9, 1, 0, 8],
+                    [0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 1, 0, 2, 0, 6, 0, 9, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0, 0],
+                    [9, 0, 6, 3, 4, 0, 5, 7, 0, 6, 0, 3, 0, 2, 1, 0, 3, 5, 7, 0, 8],
+                    [4, 0, 0, 0, 1, 0, 0, 6, 2, 0, 8, 0, 4, 3, 0, 0, 1, 0, 0, 0, 5],
+                    [0, 0, 2, 6, 0, 0, 0, 0, 3, 0, 9, 0, 8, 0, 0, 0, 0, 4, 3, 0, 0],
+                    [0, 0, 0, 2, 0, 0, 4, 9, 0, 0, 0, 0, 0, 7, 5, 0, 0, 2, 0, 0, 0],
+                    [6, 9, 1, 0, 3, 0, 2, 5, 7, 0, 0, 0, 3, 1, 4, 0, 6, 0, 5, 8, 2],
+                    [0, 7, 4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 4, 6, 0],
+                    [1, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 0, 0, 0, 3, 7, 0, 0, 0, 0, 4],
+                    [7, 4, 0, 0, 2, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 7, 3],
+                    [0, 2, 8, 0, 6, 3, 7, 0, 9, 0, 0, 0, 7, 0, 2, 1, 4, 0, 9, 5, 0]]
+
+    samurai_correct = [[9, 5, 1, 4, 6, 7, 2, 3, 8, 0, 0, 0, 9, 1, 8, 5, 6, 3, 7, 2, 4],
+                      [8, 7, 4, 1, 3, 2, 9, 6, 5, 0, 0, 0, 4, 2, 7, 9, 1, 8, 6, 5, 3],
+                      [6, 3, 2, 5, 9, 8, 7, 1, 4, 0, 0, 0, 3, 5, 6, 4, 7, 2, 8, 9, 1],
+                      [4, 1, 8, 2, 7, 6, 5, 9, 3, 0, 0, 0, 7, 9, 1, 3, 5, 4, 2, 8, 6],
+                      [5, 2, 7, 9, 1, 3, 8, 4, 6, 0, 0, 0, 8, 3, 5, 2, 9, 6, 4, 1, 7],
+                      [3, 6, 9, 8, 4, 5, 1, 7, 2, 0, 0, 0, 2, 6, 4, 7, 8, 1, 5, 3, 9],
+                      [1, 8, 3, 6, 5, 9, 4, 2, 7, 3, 5, 1, 6, 8, 9, 1, 4, 5, 3, 7, 2],
+                      [2, 4, 6, 7, 8, 1, 3, 5, 9, 8, 6, 7, 1, 4, 2, 8, 3, 7, 9, 6, 5],
+                      [7, 9, 5, 3, 2, 4, 6, 8, 1, 9, 2, 4, 5, 7, 3, 6, 2, 9, 1, 4, 8],
+                      [0, 0, 0, 0, 0, 0, 7, 9, 5, 4, 3, 8, 2, 1, 5, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 8, 1, 5, 2, 7, 6, 3, 9, 4, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 2, 3, 4, 5, 1, 9, 7, 6, 8, 0, 0, 0, 0, 0, 0],
+                      [9, 1, 6, 3, 4, 2, 5, 7, 8, 6, 4, 3, 9, 2, 1, 6, 3, 5, 7, 4, 8],
+                      [4, 3, 7, 5, 1, 8, 9, 6, 2, 1, 8, 5, 4, 3, 7, 8, 1, 9, 6, 2, 5],
+                      [8, 5, 2, 6, 9, 7, 1, 4, 3, 7, 9, 2, 8, 5, 6, 2, 7, 4, 3, 9, 1],
+                      [3, 8, 5, 2, 7, 6, 4, 9, 1, 0, 0, 0, 6, 7, 5, 4, 8, 2, 1, 3, 9],
+                      [6, 9, 1, 8, 3, 4, 2, 5, 7, 0, 0, 0, 3, 1, 4, 9, 6, 7, 5, 8, 2],
+                      [2, 7, 4, 9, 5, 1, 8, 3, 6, 0, 0, 0, 2, 9, 8, 3, 5, 1, 4, 6, 7],
+                      [1, 6, 9, 7, 8, 5, 3, 2, 4, 0, 0, 0, 5, 6, 3, 7, 9, 8, 2, 1, 4],
+                      [7, 4, 3, 1, 2, 9, 6, 8, 5, 0, 0, 0, 1, 4, 9, 5, 2, 6, 8, 7, 3],
+                      [5, 2, 8, 4, 6, 3, 7, 1, 9, 0, 0, 0, 7, 8, 2, 1, 4, 3, 9, 5, 6]]
 
     def __init__(self, rows, cols, width, height):
         self.rows = rows
@@ -47,7 +69,7 @@ class Grid:
 
     def reset(self, samurai):
         if samurai:
-            self.board = self.board1
+            self.board = self.samurai_board
             self.__init__(21, 21, 540, 540)
         else:
             self.board = self.board
@@ -187,39 +209,33 @@ class Grid:
                     return False
         return True
 
-    #MAY STILL NEED THIS
-    def sudoku_clear(self, row, col):
-        # if self.cubes[row][col].value == 0:
-        self.cubes[row][col].set_temp(0)
-
     def dlx_place(self, val):
         row, col = self.selected
         self.cubes[row][col].set(val)
         self.update_model()
     
-    def dlx_sketch(self, r, c, n, win, color=None):
+    def dlx_sketch(self, r, c, n, win, color=None, strikes=0):
         self.select(r, c)
         self.cubes[r][c].temp = n    
         self.dlx_place(self.cubes[r][c].temp)
         self.sketch(n)
-        redraw_window(win, self, 0, 0, False, color) 
-        pygame.display.update() 
-        time.sleep(0.08)  
+        redraw_window(win, self, 0, strikes, False, color) 
+        pygame.display.update()  
+        time.sleep(0.07)  
 
     def dlx_show(self, dlx, win):  
         allMoves = dlx.all_covers_uncovers
         covered = dlx.cover_or_uncover
         numMoves = int(len(allMoves) / 2)
-        numMistakes = numMoves - 81
         print('num moves:', numMoves)  # divided by 2 because every cover gets uncovered
-        print('num mistakes:', numMistakes)
+        strikes = 0
         for move in range(len(allMoves)):
-            row = allMoves[move].rowID
-            first = dlx.constraint_matrix[row-1].index(1)
-            second = dlx.constraint_matrix[row-1].index(1, 81)  # find:1  start_at:81 
+            s_row = allMoves[move].rowID
+            first = dlx.constraint_matrix[s_row-1].index(1)
+            second = dlx.constraint_matrix[s_row-1].index(1, 81)  # find:1  start_at:81 
             row = int(first / 9) 
-            col = first % 9 
-            num = int((second - 80) % 9)
+            col = first % 9  
+            num = (second - 80) % 9 
             # w/out this if, c0 would have a 9 but c1-c8 would have 0's instead of 9's
             if num == 0:
                 num = 9
@@ -228,11 +244,14 @@ class Grid:
             if self.board[row][col] != 0: 
                 continue
             elif covered[move]:
-                self.dlx_sketch(row, col, num, win, (66, 173, 245))  # (r, g, b)
+                self.dlx_sketch(row, col, num, win, (66, 173, 245), strikes)  # (r, g, b)
             elif move: 
+                strikes += 1
                 # dlx_show -> dlx_sketch -> redraw_window -> board.draw -> cubes.draw 
-                self.dlx_sketch(row, col, 0, win, (245, 66, 173))  # (r, g, b)
+                self.dlx_sketch(row, col, 0, win, (245, 66, 173), strikes)  # (r, g, b)
                 # increment mistakes count
+                #def redraw_window(win, board, time, strikes, samurai, color=None):
+
             
             if self.is_finished():
                 return 
@@ -253,6 +272,7 @@ class Cube:
 
     def draw(self, win, samurai, color=None): 
         fnt = pygame.font.SysFont("comics", 30)
+
         if not samurai:
             gap = self.width / 9
         else:
@@ -272,9 +292,95 @@ class Cube:
         if self.selected and samurai == False:
             pygame.draw.rect(win, color, (0, y, gap + self.width, gap), 3)
             pygame.draw.rect(win, color, (x, 0, gap, self.height), 3)
+        # if self.selected and samurai == True:
+        #     pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
+        #     pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+        # NEW STUFF FOR SAM SUDOKU
         if self.selected and samurai == True:
-            pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
-            pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+            gap = self.width / 27
+            # Options for center box
+            if 6 <= self.row <= 14 and 6 <= self.col <= 14:
+                if 9 <= self.col <= 11:
+                    # Center box, normal size highlight
+                    if 9 <= self.row <= 11:
+                        pygame.draw.rect(win, color, (120, y, gap + self.width - 380, gap), 3)
+                        pygame.draw.rect(win, color, (x, 120, gap, self.height - 355), 3)
+
+                    # Top three rows of center box or bottom three rows, long highlighting size
+                    if 12 <= self.row <= 14 or 6 <= self.row <= 8:
+                        #pygame.draw.rect(win, color, (0, y, gap + self.width - 125, gap), 3)
+                        pygame.draw.rect(win, color, (0, y, gap + self.width - 135, gap), 3)
+                        pygame.draw.rect(win, color, (x, 120, gap, self.height - 355), 3)
+                elif 9 <= self.row <= 11 and (6 <= self.col <= 9 or 12 <= self.col <= 14):
+                    pygame.draw.rect(win, color, (120, y, gap + self.width - 380, gap), 3)
+                    pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+                else:
+                    #pygame.draw.rect(win, color, (120, y, gap + self.width - 380, gap), 3)
+                    pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3) 
+                    pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+
+            # Top left corner box, normal size highlight
+            if 0 <= self.row <= 5 and 0 <= self.col <= 5:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 355), 3)
+
+            # Top left corner, long highlight size
+            if 6 <= self.col <= 8 and 0 <= self.row <= 5:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+            # Top left corner, annoying crap
+            if 0 <= self.col <= 5 and 6 <= self.row <= 8:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 360), 3)
+
+            # Bottom left corner box, normal size highlight
+            if 15 <= self.row <= 20 and 0 <= self.col <= 5:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 240, gap, self.height - 355), 3)
+
+            # Bottom left corner box, long highlighting
+            if 6 <= self.col <= 8 and 15 <= self.row <= 20:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+            # Bottom left corner box, annoying corner
+            if 12 <= self.row <= 14 and 0 <= self.col <= 5:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
+                pygame.draw.rect(win, color, (x, 240, gap, self.height - 360), 3)
+
+            # Top right corner box, normal size highlight
+            if 0 <= self.row <= 5 and 15 <= self.col <= 20:
+                pygame.draw.rect(win, color, (240, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 360), 3)
+
+            # Top right corner box, long highlighting size
+            if 12 <= self.col <= 14 and 0 <= self.row <= 5:
+                pygame.draw.rect(win, color, (240, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+            # Top right corner box, annoying crap
+            if 6 <= self.row <= 8 and 15 <= self.col <= 20:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 360), 3)
+
+            # Bottom right corner box, normal size highlight
+            if 15 <= self.row <= 20 and 15 <= self.col <= 20:
+                pygame.draw.rect(win, color, (240, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 240, gap, self.height - 360), 3)
+
+            # Bottom right corner box, long highlighting size
+            if 15 <= self.row <= 20 and 12 <= self.col <= 14:
+                pygame.draw.rect(win, color, (240, y, gap + self.width - 380, gap), 3)
+                pygame.draw.rect(win, color, (x, 0, gap, self.height - 120), 3)
+
+            # Bottom right corner box, annoying crap
+            if 12 <= self.row <= 14 and 15 <= self.col <= 20:
+                pygame.draw.rect(win, color, (0, y, gap + self.width - 140, gap), 3)
+                pygame.draw.rect(win, color, (x, 240, gap, self.height - 360), 3)
+        # END NEW STUFF FOR SAM SUDOKU
 
     def set(self, val):
         self.value = val
@@ -302,20 +408,24 @@ def redraw_window(win, board, time, strikes, samurai, color=None):
                      [120, button_top, 170, button_height])  # (surface, (r, g, b), [left, top, width, height])
     text = fnt.render('New Puzzle', True, (0, 0, 0))
     win.blit(text, (120, button_top))
+
     pygame.draw.rect(win, (216, 191, 216),
                      [120, button_top + 50, 375, button_height])  # (surface, (r, g, b), [left, top, width, height])
     text = fnt.render('Watch Dancing Links Do It!', True, (0, 0, 0))
     win.blit(text, (120, button_top + 50))
+
     pygame.draw.rect(win, (216, 191, 216),
                      [120, button_top + 100, 375, button_height])  # (surface, (r, g, b), [left, top, width, height])
     text = fnt.render('Quit', True, (0, 0, 0))
     win.blit(text, (120, button_top + 100))
+
     pygame.draw.rect(win, (216, 191, 216),
                      [50, button_top + 150, 100, button_height])  # (surface, (r, g, b), [left, top, width, height])
     text = fnt.render('Sudoku - Easy', True, (0, 0, 0))
     win.blit(text, (50, button_top + 150))
+
     pygame.draw.rect(win, (216, 191, 216),
-                     [200, button_top + 150, 100, button_height])  # (surface, (r, g, b), [left, top, width, height])
+                     [200, button_top + 150, 120, button_height])  # (surface, (r, g, b), [left, top, width, height])
     text = fnt.render('Sudoku - Medium', True, (0, 0, 0))
     win.blit(text, (200, button_top + 150))
 
@@ -324,7 +434,7 @@ def redraw_window(win, board, time, strikes, samurai, color=None):
     text = fnt.render('Sudoku - Hard', True, (0, 0, 0))
     win.blit(text, (350, button_top + 150))
     pygame.draw.rect(win, (216, 191, 216),
-                     [200, 600 + 150, 100, button_height])
+                     [200, 600 + 150, 120, button_height])
     text = fnt.render('Samurai - Sudoku', True, (0, 0, 0))
     win.blit(text, (200, 600 + 150))
 
@@ -419,9 +529,9 @@ def main():
                     strikes = 0
                 # dancing links button clicked
                 elif 120 <= pos[0] <= 495 and 600 <= pos[1] <= 630:
-                    samurai = False
+                    # samurai = False
                     board.reset(samurai) 
-                    dancing_links = dlx.DLX(board.board)
+                    dancing_links = dlx.DLX(board.board, samurai)
                     dancing_links.create_linked_matrix()
                     dancing_links.search(0)
                     board.dlx_show(dancing_links, win)
@@ -434,7 +544,7 @@ def main():
                     board.set_puzzle(get_puzzle(1))
                     board.reset(samurai)
                 # generate a medium puzzle
-                elif 200 <= pos[0] <= 300 and 700 <= pos[1] <= 730:
+                elif 200 <= pos[0] <= 320 and 700 <= pos[1] <= 730:
                     samurai = False
                     board.set_puzzle(get_puzzle(2))
                     board.reset(samurai)
@@ -444,7 +554,7 @@ def main():
                     board.set_puzzle(get_puzzle(3))
                     board.reset(samurai)
                 # generate a samurai puzzle
-                elif 200 <= pos[0] <= 300 and 750 <= pos[1] <= 780:
+                elif 200 <= pos[0] <= 320 and 750 <= pos[1] <= 780:
                     samurai = True
                     board.reset(samurai)
 
